4b390fc01468e0e0e05922a1f751f37d93d90a2e
solomonadzape95 <adzapesolomon@gmail.com>
2023-12-14 15:05:30 +0100

    basic part of mapty project

---
+++ 16-async-js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET','')
